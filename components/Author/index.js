9a9e933b95ccc8812fa1c42784a047ad4074acb7
zhaoyi <scoyzhao@163.com>
2020-11-09 20:14:50 +0800

    feat: change img url

--- components/Author/index.js
+++ components/Author/index.js
@@ -3,7 +3,7 @@
  * @Author: scoyzhao
  * @Date: 2020-09-29 16:13:43
  * @Last Modified by: scoyzhao
- * @Last Modified time: 2020-09-30 11:35:35
+ * @Last Modified time: 2020-11-09 20:04:31
  */
 import { Avatar, Card } from 'antd'
 import Link from 'next/link'
@@ -17,7 +17,7 @@ const Author = () => {
     <Card
       className={styles.author}
       cover={
-        <img src='http://img.wxcha.com/file/201811/01/e7acbcc1fd.jpg' />
+        <img src='https://raw.githubusercontent.com/scoyzhao/FigureBed/master/blog/index_pic.jpg' />
       }
       actions={[
         <Link href='/archive'>归档</Link>,
@@ -27,7 +27,7 @@ const Author = () => {
       ]}
     >
       <Meta
-        avatar={<Avatar src='https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png' />}
+        avatar={<Avatar src='https://raw.githubusercontent.com/scoyzhao/FigureBed/master/blog/admin/avatar.png' />}
         title='scoyzhao'
         description='前端er~'
       />
